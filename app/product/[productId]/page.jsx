2fc2fa7f96d132fd5bd85b39ff3489555169f115
Ehsan <xdehsanxd@gmail.com>
2024-11-10 22:31:19 +0330

    Add to cart button

--- app/product/[productId]/page.jsx
+++ app/product/[productId]/page.jsx
@@ -12,17 +12,24 @@ const page = async ({ params }) => {
     <section className="">
       <div className="container mx-auto p-4 pt-6 grid grid-cols-2 gap-4">
         <div className="relative w-[500px] h-[500px] justify-self-end">
-          <Image src={data.image} fill alt={data.title} />
+          <Image
+            src={data.image}
+            fill
+            alt={data.title}
+            className="rounded-xl"
+          />
         </div>
 
-        <div>
+        <div className="space-y-4 flex flex-col">
           <h1 className="text-2xl">{data.title}</h1>
-          <p>{formatCurrency(data.price)}</p>
+          <p className="font-sans text-lg">{formatCurrency(data.price)}</p>
           <p className="opacity-50">{data.category}</p>
           <hr />
           <p>Description: {data.description}</p>
 
-          <Button>Add To Cart</Button>
+          <Button className="px-5 flex z-0 mx-auto rounded-2xl btn-lg items-center gap-1 relative duration-[400ms] after:duration-500  before:block before:absolute before:w-[calc(100%+4px)] before:h-[calc(100%+4px)] before:bg-gradientBtn before:-left-[2px] before:-top-[2px] before:transition-all  before:-z-10  before:animate-[glowing_20s_ease-in-out_infinite] before:rounded-2xl  before:bg-[length:400%] before:blur-sm hover:before:blur-md after:-z-10 after:block after:absolute after:w-full after:h-full after:bg-black before:opacity-100 after:top-0 after:left-0 after:rounded-2xl hover:-translate-y-1">
+            Add To Cart
+          </Button>
         </div>
       </div>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,46 +7,55 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			primary: 'var(--primary)',
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
-  		},
-  		backgroundColor: {
-  			primary: 'var(--primary)',
-  			'primary-dark': 'var(--primary-dark)',
-  			secondary: 'var(--secondary)',
-  			card: 'var(--card)'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
-  		}
-  	}
+    extend: {
+      colors: {
+        primary: "var(--primary)",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      backgroundColor: {
+        primary: "var(--primary)",
+        "primary-dark": "var(--primary-dark)",
+        secondary: "var(--secondary)",
+        card: "var(--card)",
+      },
+      backgroundImage: {
+        gradientBtn:
+          "linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        glowing: {
+          "0%": { "background-position": "0 0" },
+          "50%": { "background-position": "400% 0" },
+          "100%": { "background-position": "0 0" },
+        },
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
 
   plugins: [require("tailwindcss-animate")],
